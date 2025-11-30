db70be5cb029ab26120c714d6bf7bdcb2f95b835
Zainab-design4346 <zainabshahzad.4346@gmail.com>
2025-11-30 16:08:07 +0400

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,4 @@
+def wishes(n):
+    print("Hello",n, "how are you?")
+    print("I hope you are doing well")
+wishes("Zainab")
---
+++ act2.py
@@ -0,0 +1,7 @@
+def weather(w,c):
+    print("Today's weather is",w)
+    print("It is",c)
+    
+weather("sunny","hot")
+weather("rainy","cold")
+weather("cloudy","cool")
